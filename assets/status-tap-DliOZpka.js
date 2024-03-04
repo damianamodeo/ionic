fa098c55e947506c0c8ff858ef5e2d272a7d5791
damianamodeo <damianamodeotheocratic@gmail.com>
2024-03-04 18:14:40 +1100

    Updates

--- assets/status-tap-DliOZpka.js
+++ assets/status-tap-DliOZpka.js
@@ -1,3 +1,3 @@
-import{r as a,f as i,a as c,w as d,s as l}from"./index-DesyZWD6.js";/*!
+import{r as a,f as i,a as c,w as d,s as l}from"./index-DW3xlLJH.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
